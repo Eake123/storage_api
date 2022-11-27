c9401cbba55c35d6183c3bb12ad7a175ae1f5e76
Eake123 <82546019+Eake123@users.noreply.github.com>
2022-11-27 12:03:04 -0700

    Update cloud_api.py

--- cloud_api.py
+++ cloud_api.py
@@ -1,23 +1,24 @@
 import requests
 import base64
 import json
+
 class Clout_Api:
-    def __init__(self) -> None:
+    def __init__(self,password) -> None:
         self.__sess = requests.Session()
-    
+        self.header = {
+            'Content-Type':'application/json',
+            'PASSWORD':password
+        }
 
 
     def __url(self,*args,data=None,method='get'):
         url = '' + '/'.join(args)
-        header = {
-            'Content-Type':'application/json'
-        }
         if method == 'get':
-            r = self.__sess.get(url,data=data,headers=header)
+            r = self.__sess.get(url,data=data,headers=self.header)
         elif method == 'post':
-            r = self.__sess.post(url,data,headers=header)
+            r = self.__sess.post(url,data,headers=self.header)
         elif method == 'delete':
-            r = self.__sess.delete(url,data=data,headers=header)
+            r = self.__sess.delete(url,data=data,headers=self.header)
         return r
 
     def download(self,root,folder,file_name,file):
@@ -25,6 +26,17 @@ class Clout_Api:
             b = file.read()
             tb = base64.b64encode(b)
             tb_str = tb.decode('utf-8')
+        params = {
+                'root':root,
+                'folder':folder,
+                'file_name':file_name,
+                'file':tb_str
+        }
+        return self.__url(
+            'clout',
+            data=json.dumps(params),
+            method='post'
+            )
         r = self.__sess.post(
             BASE + 'clout',
             data=json.dumps({
@@ -84,7 +96,45 @@ class Clout_Api:
                 file.write(data)
             r = file_name
         return r
+    
+    def powershell(self,cmd_str):
+        data = {
+            'cmd_str':cmd_str
+        }
+        return self.__url(
+            'powershell',
+            data=json.dumps(data),
+            method='post'
+        )
+    def cmd(self,cmd_str):
+        data = {
+            'cmd_str':cmd_str
+        }
+        return self.__url(
+            'cmd',
+            data=json.dumps(data),
+            method='post'
+        )
 if __name__ == '__main__':
-    c = Clout_Api()
-
-
+    c = Clout_Api(password)
+    # r = c.download(
+    #     'c',
+    #     'programs',
+    #     'clout.bat',
+    # )
+    # print(c.subdirectory('c','programs'))
+    r = c.cmd(
+    )
+    # r = c.delete(
+    #     'c',
+    #     'programs',
+    #     'test'
+    # )
+    # for i in r['files']:
+    #     c.delete(
+    #         'c',
+    #         'misc',
+    #         i
+    #     )
+    # r = c.subdirectory('c','misc')
+    print(r)
